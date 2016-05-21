7a174d642d42f6ca9c09287c523434b9378a9512
Sergey Zhiltsov <sergey.zhiltsov@sigma.software>
2016-05-21 14:06:34 +0300

    Updated vagrant image

--- ft_api/pysetup.sh
+++ ft_api/pysetup.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-VENV_PATH=../env
+VENV_PATH=env
 
 rm -rf $VENV_PATH
-virtualenv -p python3 $VENV_PATH
+virtualenv --python python3 $VENV_PATH
 source $VENV_PATH/bin/activate
 
 pip install -r ./settings/requirements.txt
