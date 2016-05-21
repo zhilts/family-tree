63060c1da936da6a6cd973dde1c5b53f1020c230
Sergey Zhiltsov <sergey.zhiltsov@sigma.software>
2016-05-21 14:27:22 +0300

    Updated fabfiles

--- fabfile.py
+++ fabfile.py
@@ -2,8 +2,16 @@ from fabric.context_managers import lcd
 from fabric.decorators import task
 from fabric.operations import local
 
+API_PATH = 'ft_api'
+
 
 @task()
 def pysetup():
     with lcd('./ft_api'):
         local('fab pysetup')
+
+
+@task()
+def ft(command):
+    with lcd(API_PATH):
+        local('fab {command}'.format(command))
--- ft_api/fabfile.py
+++ ft_api/fabfile.py
@@ -5,3 +5,34 @@ from fabric.operations import local
 @task()
 def pysetup():
     local('./pysetup.sh')
+
+
+@task()
+def runserver():
+    manage("runserver 0.0.0.0:8000")
+
+
+@task()
+def manage(command):
+    local('./pyenv.sh ./manage.py {command}'.format(command=command))
+
+
+@task
+def load_data(path):
+    manage('loaddata {path}'.format(path=path))
+
+
+@task()
+def makemigrations():
+    manage('makemigrations')
+
+
+@task()
+def migrate():
+    manage('migrate --run-syncdb')
+
+
+@task(alias='rdb')
+def recreate_database():
+    local('rm -rf ./db.sqlite3')
+    migrate()
